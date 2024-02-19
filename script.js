17ca8796ffe9a6a9cd661270594e45232b79c7e8
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-02-18 23:28:16 -0500

    Fetch con 1:30 de Fetching and Storing Data

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-import { loadContent } from "./src/loader.js";
+// import { loadContent } from "./src/loader.js";
 
-loadContent();
+// loadContent();
+const base_uri = "https://keepable-api.herokuapp.com";
+
+const url = `${base_uri}/login`;
+const credentials = {
+  username: "codeable",
+  password: "123456",
+};
+const response = fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: JSON.stringify(credentials),
+});
+// console.log(response);
+// response.then((data) => console.log(data));
+// response.then((data) => console.log(data));
+response
+  .then((data) => data.json())
+  .then((result) => {
+    if (Array.isArray(result)) throw new Error(result.join(","));
+
+    const token = result.token;
+    console.log(token);
+  })
+  .catch((error) => console.log(error.message)); //Esto permite que cuando haya error vengan a este línne a de código garcias al if de la línea 25
